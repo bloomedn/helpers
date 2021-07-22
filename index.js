65da2529fbf4c1732bec30a7bfe5a9f35900c02e
Suman Chapai <sumanchapai@gmail.com>
2021-07-22 12:53:10 +0545

    last 50 years

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ export default function dateFormat(dtime) {
     const offDtime = new Date(dtime);
     return `${offDtime.toDateString()}`;
 }
+
+export function last50Years() {
+    const thisYear = new Date().getFullYear();
+    let beginningYear = thisYear - 50;
+    let years = [];
+    for (let i = thisYear; i >= beginningYear; i = i - 1) {
+        years.push(i);
+    }
+    return years;
+}
